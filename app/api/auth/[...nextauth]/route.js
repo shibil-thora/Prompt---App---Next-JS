3af1f1dd31305b2272a39e4822240381ed01cadb
shibil-thora <shibilallakkat123@gmail.com>
2024-09-19 09:02:41 +0530

    added functionalities of signin

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"; 
 import GoogleProvider from 'next-auth/providers/google'
+import { connectToDB } from "@/utils/database";
+import User from "@/models/user";
 
 
 const handler = NextAuth({
@@ -10,13 +12,32 @@ const handler = NextAuth({
         })
     ], 
     async session({ session }) {
-
+        const sessionUser = await User.findOne({
+            email: session.user.email,
+        })
     }, 
     async signIn({ profile }) {
         try{
+            await connectToDB(); 
 
+            // check if a user already exists 
+            const userExists = await User.findOne({
+                email: profile.email, 
+            })
+
+            // if not create a new user 
+            if(!userExists) {
+                await User.create({
+                    email: profile.email, 
+                    username: profile.name.replace(" ", "").toLowerCase(), 
+                    image: profile.picture, 
+                })
+            }
+
+            return true; 
         }catch(error) {
-            
+            console.log(error)
+            return false;
         }
     }
 })
